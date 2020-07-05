1e66ddeeacd09a22ed6545cbe086cd95b999a660
QadirAshraf <aqadir@rfamec.com>
2020-07-04 22:57:34 -0500

    cross compile for imx8 with cmake

---
+++ imx8-Hummingboard/HelloWorldCrossCompile/source.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+std::cout<<"check if cross compile works..."<<std::endl;
+return 0;
+}
---
+++ imx8-Hummingboard/HelloWorldCrossCompile/toolchain/imx8_toolchain.cmake
@@ -0,0 +1,12 @@
+set(CMAKE_SYSTEM_NAME Linux)
+
+set(CMAKE_SYSROOT /home/ashraf/workspace/imx8ImageBuild/sysroot-glibc-linaro-2.23-2017.05-aarch64-linux-gnu)
+set(CMAKE_STAGING_PREFIX /home/ashraf/workspace/imx8ImageBuild/stage)
+
+set(CMAKE_C_COMPILER /home/ashraf/workspace/imx8ImageBuild/gcc-linaro-6.3.1-2017.05-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-gcc)
+set(CMAKE_CXX_COMPILER /home/ashraf/workspace/imx8ImageBuild/gcc-linaro-6.3.1-2017.05-i686_aarch64-linux-gnu/bin/aarch64-linux-gnu-g++)
+
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(CMAKE_FIND_ROOT_PATH_MODE_LIBRARY ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_INCLUDE ONLY)
+set(CMAKE_FIND_ROOT_PATH_MODE_PACKAGE ONLY)
